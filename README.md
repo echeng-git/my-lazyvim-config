620ce179a00c1fcf4f85ecd0ed3b0adf4319cf0e
echeng-git <eccheng@ualberta.ca>
2025-01-30 17:25:26 -0700

    <C+BS> and readme

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # 💤 LazyVim
 
-A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
-Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+My template for [LazyVim](https://github.com/LazyVim/LazyVim). [(documentation goes here)](https://lazyvim.github.io/installation)
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Ctrl+Backspace - delete previous in insert mode
+vim.api.nvim_set_keymap("i", "<C-h>", "<C-w>", { noremap = true, silent = true })
