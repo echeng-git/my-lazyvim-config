51460fcaf7050a94d5c5e75cfdf5b6be6597ed9a
echeng-git <eccheng@ualberta.ca>
2025-02-12 11:13:16 -0700

    disable MD013

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,10 +1,19 @@
 return {
   {
-
     "iamcco/markdown-preview.nvim",
     init = function()
       vim.g.mkdp_refresh_slow = 1
       vim.g.mkdp_auto_close = 0
     end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        markdownlint = {
+          args = { "--disable", "MD013", "--" },
+        },
+      },
+    },
   },
 }
